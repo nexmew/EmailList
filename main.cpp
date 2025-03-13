0464f64689d66a691080420a9a700e95e81b6bb6
Scott <43623351+nexmew@users.noreply.github.com>
2025-03-12 21:51:58 -0700

    completed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
+using namespace std;
+
+int main() {
+  
+    vector<string> emails;
+    string email;
+
+    cout << "Enter emails (type 'done' to finish):" << endl;
+
+    // Take user input 
+    // when they type "done" end it
+    while (true) {
+        getline(cin, email);
+        if (email == "done") break; 
+        emails.push_back(email);
+    }
+
+    // Sort the emails alphabetically
+    // uses algorithm library
+    sort(emails.begin(), emails.end());
+
+    cout << endl << "Sorted Emails:" << endl;
+  
+    // output of emails
+    for (const string& e : emails) {
+        cout << e << endl;
+    }
+
+    return 0;
+}
